479ec923b47514f2f7d0a75cab429e7280297583
Pavel Kim <kimpauevg@mail.ru>
2023-09-24 22:03:04 +0300

    working rushjs example

--- rushjs-example-2/about/src/bootstrap.tsx
+++ rushjs-example-2/about/src/bootstrap.tsx
@@ -1,4 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
